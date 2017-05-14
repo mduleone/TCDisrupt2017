b042487ca5e92877f1b5a582fe822fbc5533f0a9
Matt DuLeone <matt@duleone.com>
2017-05-14 07:24:36 -0400

    Remove dependency on database

--- server/config/database.js
+++ server/config/database.js
@@ -1,18 +1,18 @@
 const pg = require('pg');
 
 function scaffoldDatabase(app) {
-    const connectionObject = {
-        port: app.env.DB_PORT,
-        host: app.env.DB_HOST,
-        database: app.env.DB_NAME,
-        user: app.env.DB_USER,
-        password: app.env.DB_PASSWORD
-    };
-
-    const dbClient = new pg.Client(connectionObject);
-    dbClient.connect();
-
-    app.db = dbClient;
+    //const connectionObject = {
+    //    port: app.env.DB_PORT,
+    //    host: app.env.DB_HOST,
+    //    database: app.env.DB_NAME,
+    //    user: app.env.DB_USER,
+    //    password: app.env.DB_PASSWORD
+    //};
+    //
+    //const dbClient = new pg.Client(connectionObject);
+    //dbClient.connect();
+    //
+    //app.db = dbClient;
 }
 
 module.exports = scaffoldDatabase;
