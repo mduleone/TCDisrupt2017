d808753f2e5615b40de399fa15145c6146777e21
Matt DuLeone <matt@duleone.com>
2017-05-13 21:45:24 -0400

    add comments

--- server/routes/api.js
+++ server/routes/api.js
@@ -3,8 +3,12 @@ const db = require('../db');
 
 const app = new Router();
 
-app.get('/', (req, res) => {
-    console.log(db);
+app.get('/:userId', (req, res) => {
+    // console.log(db);
+    // db is the database instance. Check out the pg docs: https://www.npmjs.com/package/pg
+    //
+    // console.log(req.params.userId);
+    // access query params at req.params.PARAM
     return res.status(200).json({'status': 'ok'});
 });
 
